062d19ac85f9f1091c228fbf8e3f3191a2a5ba9c
Bilikis Orulebaja <bilkisbalogun@gmail.com>
2018-10-02 05:59:39 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -28,7 +28,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   
   holiday_hash[:winter].each do |holiday_hash, items|
     items << supply
-    puts items
+    #puts items
     end
 end
 
